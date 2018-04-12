838ca9508e464c107e2d9802507a5b015ffe0269
Jin Yuliang <530367387@qq.com>
2018-04-12 18:36:29 -0500

    update jacobi relaxation bugs

--- src/main/java/com/yuliang/tutorial/mum/parallelprogramming/part3/SequentialJacobiRelaxation.java
+++ src/main/java/com/yuliang/tutorial/mum/parallelprogramming/part3/SequentialJacobiRelaxation.java
@@ -1,38 +1,37 @@
 package com.yuliang.tutorial.mum.parallelprogramming.part3;
+import java.util.Date;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class SequentialJacobiRelaxation {
 
-    public static final int n = 12;
-    public static final double tolerance = 0.1;
+    public static final int n = 10002;
+    public static final float tolerance = 0.1f;
     public static final int threadNums = Runtime.getRuntime().availableProcessors();
 
-    static double [][] a = new double[n][n], b = new double[n][n];
-    static double [][] a1 = new double[n][n], b1 = new double[n][n];
+    static float[][] a = new float[n][n], b = new float[n][n];
+    static float[][] a1 = new float[n][n], b1 = new float[n][n];
 
     public static void main(String[] args) {
         initializeData();
         sequential(a, b);
         parallel(a1,b1);
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                System.out.printf("%5.1f", a1[i][j]);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(a[i][j] != a1[i][j]) {
+                    System.out.print("The result is not equal." + "i: " +i + "j:" + j);
+                    System.out.print("a[i][j]=" + a[i][j]);
+                    System.out.print("a1[i][j]=" + a1[i][j]);
+                }
             }
-            System.out.println();
         }
-
-
     }
 
-    private static void parallel(double[][] a1, double[][] b1) {
+    private static void parallel(float[][] a1, float[][] b1) {
+        Date start = new Date();
         SCAN_ARRAY[] tasks = new SCAN_ARRAY[threadNums];
+        Thread[] threads = new Thread[threadNums];
         Aggregate aggregate = new Aggregate(a1, b1, tolerance, tasks);
         CyclicBarrier barrier = new CyclicBarrier(threadNums);
         CyclicBarrier checkBarrier = new CyclicBarrier(threadNums, aggregate);
@@ -47,17 +46,34 @@ public class SequentialJacobiRelaxation {
             }
         }
 
-        for (SCAN_ARRAY task : tasks) {
-            new Thread(task).start();
+        for(int i=0;i<threadNums;i++) {
+            threads[i] = new Thread(tasks[i]);
         }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Date end = new Date();
+        System.out.println("Parallel solution used time: " + (end.getTime() - start.getTime()));
     }
 
-    private static void sequential(double[][] a, double[][] b) {
+    private static void sequential(float[][] a, float[][] b) {
+        Date start = new Date();
         boolean done;
+        int count = 0;
         do {
             for(int i=1;i<n-1;i++) {
                 for(int j=1;j<n-1;j++) {
-                    b[i][j] = (a[i-1][j] + a[i-1][i+1] + a[i][j-1] + a[i][j+1]) / 4.0;
+                    b[i][j] = (a[i-1][j] + a[i+1][j] + a[i][j-1] + a[i][j+1]) / 4.0f;
                 }
             }
 
@@ -71,45 +87,49 @@ public class SequentialJacobiRelaxation {
                     a[i][j] = b[i][j];
                 }
             }
+            count++;
         } while (!done);
+        Date end = new Date();
+        System.out.println("For sequential solution the loop logic runs times: " + count);
+        System.out.println("Parallel solution used time: " + (end.getTime() - start.getTime()));
     }
 
     private static void initializeData() {
         for(int i=1;i<n - 1 ;i++) {
             for(int j=1;j<n -1;j++) {
-                a[i][j] = 0.0;
-                a1[i][j] = 0.0;
+                a[i][j] = 0.0f;
+                a1[i][j] = 0.0f;
             }
         }
 
         for(int i=0; i<n; i++) {
-            a[0][i] = 10.0;
-            a[n-1][i] = 10.0;
-            a1[0][i] = 10.0;
-            a1[n-1][i] = 10.0;
+            a[0][i] = 10.0f;
+            a[n-1][i] = 10.0f;
+            a1[0][i] = 10.0f;
+            a1[n-1][i] = 10.0f;
         }
 
         for(int i=0; i<n; i++) {
-            a[i][0] = 10.0;
-            a[i][n-1] = 10.0;
-            a1[i][0] = 10.0;
-            a1[i][n-1] = 10.0;
+            a[i][0] = 10.0f;
+            a[i][n-1] = 10.0f;
+            a1[i][0] = 10.0f;
+            a1[i][n-1] = 10.0f;
         }
 
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
-                b[i][j] = 0.0;
-                b1[i][j] = 0.0;
+                b[i][j] = 0.0f;
+                b1[i][j] = 0.0f;
             }
         }
 
-        for (double[] doubles : a) {
-            for (double aDouble : doubles) {
-                System.out.print(aDouble + " ");
-            }
-            System.out.println();
-        }
+//        for (float[] doubles : a) {
+//            for (double aDouble : doubles) {
+//                System.out.print(aDouble + " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
 
@@ -119,8 +139,8 @@ class SCAN_ARRAY implements Runnable {
     Boolean done;
     CyclicBarrier barrier;
     CyclicBarrier checkBarrier;
-    double [][] a, b;
-    double tolerance;
+    float [][] a, b;
+    float tolerance;
 
     public Boolean getDone() {
         return done;
@@ -130,7 +150,7 @@ class SCAN_ARRAY implements Runnable {
         this.done = done;
     }
 
-    public SCAN_ARRAY(int start, int end, double[][] a, double[][] b, CyclicBarrier barrier, CyclicBarrier checkBarrier, double tolerance) {
+    public SCAN_ARRAY(int start, int end, float[][] a, float[][] b, CyclicBarrier barrier, CyclicBarrier checkBarrier, float tolerance) {
         this.start = start;
         this.end = end;
         this.a = a;
@@ -176,13 +196,13 @@ class Aggregate implements Runnable {
 
     volatile int count = 0;
 
-    double tolerance;
+    float tolerance;
 
-    double[][] a, b;
+    float[][] a, b;
 
     SCAN_ARRAY[] tasks;
 
-    public Aggregate(double[][] a, double[][] b, double tolerance, SCAN_ARRAY[] tasks) {
+    public Aggregate(float[][] a, float[][] b, float tolerance, SCAN_ARRAY[] tasks) {
         this.a = a;
         this.b = b;
         this.tolerance = tolerance;
@@ -204,7 +224,7 @@ class Aggregate implements Runnable {
         }
 
         if(done) {
-            System.out.println("The thread runs times: " + count);
+            System.out.println("The aggregator thread runs times: " + count);
         }
     }
 }
